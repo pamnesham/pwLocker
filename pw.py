348109915b594039647f81770dc9ec89c58e439a
pamnesham <nesha002@umn.edu>
2017-09-12 14:32:55 -0500

    Added instructions for using with batch file

--- pw.py
+++ pw.py
@@ -1,13 +1,13 @@
 #! python3
 # An insecure password locker program
 passwords = {'school': 'abc123',
-            'blog': 'cat45'
+            'blog': 'cat45',
             'luggage': '67576'}
-
- import sys
- if len(sys.argv) < 2:
-     print('Usage: python pw.py [account] - copy account password')
-     sys.exit()
+import sys
+import pyperclip
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
 account = sys.argv[1]   # first command line arg is the acct name
 
 if account in passwords:
@@ -15,3 +15,7 @@ if account in passwords:
     print('Password for ' + account + ' copied to clipboard.')
 else:
     print('There is no account named ' + account)
+
+# Create a batch file to run from 'run' window:
+'''@py.exe C:\path_to_this_file\pw.py %*
+@pause'''
